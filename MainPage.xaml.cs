5135f7bc1bc922f243a93cb428b1dc12544b9593
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-01-28 01:41:36 -0500

    Display int sample value and size when the user selects int in the type list view.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,7 +9,28 @@ public partial class MainPage : ContentPage
 
     private void OnTypeSelected(object sender, SelectedItemChangedEventArgs e)
     {
-        
+        string typeName = e.SelectedItem as string;
+        switch (typeName)
+        {
+            case "int":
+                ShowIntValue();
+                break;
+        }
+    }
+
+    private void ShowIntValue()
+    {
+        //declare an int local variable
+        int intVar;
+
+        //initialize the variable to a sample value
+        intVar = -125;
+
+        //display the sample value
+        _txtSampleValue.Text = intVar.ToString();
+
+        //display the size of the variable in bytes
+        _txtSize.Text = sizeof(int).ToString();
     }
 
     private void OnClearTypeInfo(object sender, EventArgs e)
